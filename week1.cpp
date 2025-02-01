b27a0cc2fe56ff796b03b22f5f3cc1ad2b304c02
Gursamer <mangatgursamer@gmail.com>
2025-01-31 22:18:03 -0800

    Add files via upload

---
+++ week1.cpp
@@ -0,0 +1,24 @@
+// #include <iostream>
+
+// int prog(int n) {
+//     return 4 + 3*(n-1);
+// }
+
+// int SumProg(int n) {
+//  if (n == 1) {
+//         return 4;
+//     }
+//     return SumProg(n - 1) + prog(n);
+// }
+
+
+
+// int main() {
+//     for(int i = 0; i<=5; i++) {
+//         std::cout << prog(i) << " ";
+//     }
+
+//     std::cout << std::endl;
+//     std::cout << SumProg(43);
+//     return 0;
+// }
